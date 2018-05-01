ea8e6d0de29f45894f8524584a969a473bea51e3
Alexander M. Turek <me@derrabus.de>
2018-05-02 01:37:04 +0200

    Switched to Entity user provider.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,13 +3,21 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table("users")
  */
-class User
+class User implements AdvancedUserInterface
 {
+    private const USERLEVEL_MAP = [
+        4 => 'ROLE_ADMIN',
+        3 => 'ROLE_SUPER_MODERATOR',
+        2 => 'ROLE_MODERATOR',
+        1 => 'ROLE_USER',
+    ];
+
     /**
      * @var int
      *
@@ -45,39 +53,129 @@ class User
         return $this->id;
     }
 
-    public function getUsername(): string
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
     {
-        return $this->username;
+        return $this->level > -1;
     }
 
-    public function setUsername(string $username): User
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
     {
-        $this->username = $username;
-
-        return $this;
+        return true;
     }
 
-    public function getPassword(): string
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {
+        return true;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return [self::USERLEVEL_MAP[$this->level] ?? 'ROLE_USER'];
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
     {
         return $this->password;
     }
 
-    public function setPassword(string $password): User
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
     {
-        $this->password = $password;
-
-        return $this;
+        return null;
     }
 
-    public function getLevel(): int
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
     {
-        return $this->level;
+        return $this->username;
     }
 
-    public function setLevel(int $level): User
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
     {
-        $this->level = $level;
-
-        return $this;
     }
 }
